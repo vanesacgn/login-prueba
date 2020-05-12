21f41112ed40d761d9b9f4cd861a6e11545bcdff
Vanesa CG <vanesacgn@gmail.com>
2020-05-12 05:29:14 -0500

    Add files via upload

---
+++ bienvenido.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+	<?php require_once "scripts.php"; ?>
+</head>
+<body>
+<br><br><br>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="jumbotron">
+				<a href="php/salir.php" class="btn btn-info">Cerrar sesión</a>
+					<h2>Bienvenido</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
