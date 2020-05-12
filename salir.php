99a67673da20e721c3da0d80ffedbb17bef22961
Vanesa CG <vanesacgn@gmail.com>
2020-05-12 05:32:28 -0500

    Delete salir.php

--- salir.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    session_start(); //Iniciar una nueva sesión o reanudar la existente
-    session_destroy(); //Destruye la sesión
-  
-    header('location: index.php'); //Redirecciona la inicio
-?>
